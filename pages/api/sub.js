9aa967de9e14b3dc28bb3256255285d45d234b6f
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-05-21 17:22:12 +0530

    api working!

--- pages/api/sub.js
+++ pages/api/sub.js
@@ -1,9 +1,21 @@
-export default function subscribe(req, res) {
+import { sendEmail } from "@/app/utils/novu"
+
+export default async function subscribe(req, res) {
     try {
-        res.status(200).json({ message: 'working!' })
+        // res.status(200).json({ message: 'working!' })
+        if (req.method === 'POST') {
+            const { email, description } = req.body
+            await sendEmail(email,description)
+            await res.status(200).json({ message: 'email working' })
+        }
+
     } catch (error) {
+        console.log(error);
         res.status(405).json({ message: 'not working' })
     }
+
+
+
 
     // if (req.method === 'GET') {
     //     res.status(200).json({ message: 'works' })
---
+++ src/app/utils/novu.js
@@ -0,0 +1,25 @@
+import { Novu } from '@novu/node';
+
+const novu = new Novu(process.env.NOVU_API_KEY);
+
+export async function sendEmail(email, description) {
+
+    await novu.subscribers.identify('123', {
+        email: email,
+        firstName: "Subscriber",
+    });
+
+
+    await novu.trigger('email-quickstart', {
+
+
+        to: {
+            subscriberId: '123456',
+            email: email
+        },
+        payload: {
+            email: email,
+            description: description
+        }
+    });
+}
