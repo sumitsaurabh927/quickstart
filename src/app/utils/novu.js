b89b18bfe3aa2eeb0d77b8234ea35eab6a350662
Sumit Saurabh <62152915+sumitsaurabh927@users.noreply.github.com>
2023-05-25 22:01:22 +0530

    topics working!

--- src/app/utils/novu.js
+++ src/app/utils/novu.js
@@ -3,18 +3,13 @@ import { Novu } from '@novu/node';
 const novu = new Novu(process.env.NOVU_API_KEY);
 
 export async function sendEmail(email, description) {
-
     await novu.subscribers.identify('123', {
         email: email,
         firstName: "Subscriber",
     });
-
-
     await novu.trigger('email-quickstart', {
-
-
         to: {
-            subscriberId: '123456',
+            subscriberId: '123',
             email: email
         },
         payload: {
